da0c6de8057e8f4c9240807573c3c64a0842a6fc
vantage-ola <vantageola4@gmail.com>
2022-10-02 23:19:57 +0100

    +new font,change to export default

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,8 +3,10 @@ import { Wrapper, Content } from "./Footer.styles";
 
 const year = new Date().getFullYear()
 
-export const Footer = () => {
+const Footer = () => {
     return <Wrapper><p>&copy;{year} Everyday People |  
       fan art by <Content href="https://twitter.com/opening_petals" target="_blank" rel="noopener noreferrer" >dami</Content> | made with <span >&#9829;</span> by <Content href="https://github.com/vantage-ola" target="_blank"  rel="noopener noreferrer" >ola</Content>
     </p></Wrapper>
 }
+
+export default Footer
