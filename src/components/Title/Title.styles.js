03ebdf4020bd3dcaa77669fa90fb5db7d530db06
vantage-ola <vantageola4@gmail.com>
2022-10-03 22:57:19 +0100

    + Title.js

---
+++ src/components/Title/Title.styles.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+
+export const Content = styled.h1`
+    font-family: 'Footlight MT Pro', sans-serif;
+    font-style: normal;
+    font-size: 120px;
+    font-weight: 300;
+    width: 465px;
+    height: 220px;
+    line-height: 110px;
+    margin-left: 140px;
+
+`
+export const Wrapper = styled.div`
+    position: relative;
+    bottom: 40px;
+`
+export const TitleP = styled.p`
+    font-size: 14px;
+    margin-left: 150px;
+    line-height: 11px;
+`
+export const LearnMore = styled.a`
+    color: #FF931E;
+    text-decoration: none;
+    font-weight: bold;
+    cursor: pointer;        
+    &:hover {
+    opacity: 0.5;
+  }
+
+
+`
---
+++ src/components/Title/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Content, Wrapper,TitleP, LearnMore } from "./Title.styles";
+
+const Title = () => {
+    return (
+        <>
+
+            <Content>
+            3v3ryday
+            p3opl3
+            </Content>
+        <Wrapper>
+            <TitleP>Everyday people is an NFT project that tells the tale of everyday degens.</TitleP>
+            <TitleP>It’s supply is capped at 3333 artworks.</TitleP>
+            <TitleP>It is also brought to you by Hangout DAO.</TitleP>
+            <TitleP><LearnMore href="https://twitter.com/3V3RYDAYP3OPL3" target="_blank" rel="noopener noreferrer" >Learn More</LearnMore></TitleP>
+        </Wrapper>
+        </>
+
+        
+    )
+}
+export default Title
