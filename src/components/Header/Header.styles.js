bf5fa417cf2da9641dc0c807579029aba39b42c3
vantage-ola <vantageola4@gmail.com>
2022-10-03 22:57:36 +0100

    +Header

---
+++ src/components/Header/Header.styles.js
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+
+    flex-wrap: wrap;
+    position: relative;
+    padding: 0rem 2rem;
+    top: 0;
+    left: 0;
+    right: 0;
+`
+export const Content = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    max-width: 1660px;
+    padding: 10px 0;
+    margin: 0 auto;
+`
+export const EdgeImg = styled.img`
+    width: 91.33px;
+    height: 94px;
+    left: 27px;
+    top: 25px;
+    border-radius: 50%;
+    
+`
+export const EdgeInfo = styled.a`
+    padding: 0 1.3rem;
+    font-size: 32px;
+    font-weight: bold;
+    position: relative;
+    right: 100px;
+    bottom: 20px;
+`
+export const EdgeLower = styled.a`
+
+    font-weight: 700;
+    font-size: 14px;
+    position: relative;
+    top: 20px;
+    right: 65px;
+    
+
+`
+export const Edge = styled.div`
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+`;
---
+++ src/components/Header/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Wrapper, Content, EdgeImg, Edge, EdgeInfo, EdgeLower } from "./Header.styles";
+import img from '../../img/image12.jpeg'
+
+const Header = () => {
+    return (
+        <Wrapper>
+            <Content>
+                <EdgeImg src={img} />
+                <Edge>
+                    <EdgeInfo>3K+<EdgeLower>COLLECTION</EdgeLower></EdgeInfo>
+                    <EdgeInfo>5+<EdgeLower className="five">TEAM MEMBERS</EdgeLower></EdgeInfo>               
+                </Edge>
+            </Content>
+        </Wrapper>
+    )
+
+}
+export default Header;
