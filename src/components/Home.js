f0a8f5a0b421ddce73792f4316687ca206b3b34e
vantage-ola <vantageola4@gmail.com>
2022-10-07 14:23:17 +0100

    + home

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import Footer from "./Footer";
 import Title from "./Title";
 import Header from "./Header";
 import People from "./People";
+import Slideshow from "./SlideShow";
 
 const Home = () =>  {
     return (
@@ -9,6 +10,9 @@ const Home = () =>  {
         <Header/>
         <Title/> 
         <People/>
+        <section>
+            <Slideshow/>
+        </section>
         <Footer/>
         </>
     );
