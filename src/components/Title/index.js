ae3f8fb52a14d7eb67d4f5420c46d79be4e55651
vantage-ola <vantageola4@gmail.com>
2022-10-08 14:40:24 +0100

    final commit

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -12,7 +12,7 @@ const Title = () => {
         <Wrapper>
             <TitleP>Everyday people is an NFT project that tells the tale of everyday degens.</TitleP>
             <TitleP>It’s supply is capped at 3333 artworks.</TitleP>
-            <TitleP>It is also brought to you by Hangout DAO.</TitleP>
+            <TitleP>It is also brought to you by <LearnMore href="https://twitter.com/hangoutDao_" target="_blank" rel="noopener noreferrer" >Hangout DAO.</LearnMore></TitleP>
             <TitleP><LearnMore href="https://twitter.com/3V3RYDAYP3OPL3" target="_blank" rel="noopener noreferrer" >Learn More</LearnMore></TitleP>
         </Wrapper>
         </>
